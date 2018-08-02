fba1f46b3742575f58af7f610d09636342598db4
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-08-02 13:16:18 +0300

    added storyboard view

---
+++ HR-helper/HR-helper/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HR-helper
+//
+//  Created by Alina Zaitseva on 8/2/18.
+//  Copyright © 2018 Alina Zaitseva. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
